a15d920a64efb40e8b5056394ae1adc91f4295a5
MrRomashkaaa <mrromashkaofficial@gmail.com>
2024-02-22 16:49:02 +0100

    update

--- src/main/java/homeworkModule11/Task1.java
+++ src/main/java/homeworkModule11/Task1.java
@@ -2,25 +2,20 @@ package homeworkModule11;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Task1 {
-    public static String formatNames(List<String> names) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < names.size(); i++) {
-            if (i % 2 == 0) {
-                result.append((i + 1)).append(". ").append(names.get(i)).append(", ");
-            }
-        }
-
-        if (result.length() > 0) {
-            result.delete(result.length() - 2, result.length());
-        }
-        return result.toString();
-    }
-
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Ivan", "Peter", "Roman", "Oleg", "Oleksandr");
-        String formattedNames = formatNames(names);
-        System.out.println(formattedNames);
+        String result = filterAndFormatNames(names);
+        System.out.println(result);
+    }
+
+    public static String filterAndFormatNames(List<String> names) {
+        return IntStream.range(0, names.size())
+                .filter(i -> i % 2 == 0) // фільтруємо непарні індекси
+                .mapToObj(i -> (i + 1) + ". " + names.get(i))
+                .collect(Collectors.joining(", "));
     }
 }
